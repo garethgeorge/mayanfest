8834e9d3d685b87f56d866f5faa3c0b6018197df
Nazmus Saquib <saquib2527@yahoo.com>
2018-10-20 14:17:56 -0700

    add resolve indirection to INode

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -15,26 +15,126 @@ struct FileSystemException : public std::exception {
 };
 
 struct INode {
+	static constexpr uint64_t DIRECT_ADDRESS_COUNT = 8;
+	static constexpr uint64_t INDIRECT_ADDRESS_COUNT = 1;
+	static constexpr uint64_t DOUBLE_INDIRECT_ADDRESS_COUNT = 1;
+	static constexpr uint64_t ADDRESS_COUNT = DIRECT_ADDRESS_COUNT + INDIRECT_ADDRESS_COUNT + DOUBLE_INDIRECT_ADDRESS_COUNT;
+	static uint64_t INDIRECT_TABLE_SIZES[] = {DIRECT_ADDRESS_COUNT, INDIRECT_ADDRESS_COUNT, DOUBLE_INDIRECT_ADDRESS_COUNT};
+	
 	uint64_t UID = 0; //user id
 	uint64_t last_modified = 0; //last modified timestamp
 	uint64_t file_size = 0; //size of file
 	uint64_t reference_count = 0; //reference count to the inode
-	uint64_t direct_addresses[8] = {0}; //8 direct
-	uint64_t indirect_addresses[1] = {0}; //1 indirect
-	uint64_t double_indirect_addresses[1] = {0}; //1 indirect
+	uint64_t addresses[ADDRESS_COUNT] = {0}; //8 direct
+	uint64_t direct_addresses[DIRECT_ADDRESS_COUNT] = {0}; //8 direct
+	uint64_t indirect_addresses[INDIRECT_ADDRESS_COUNT] = {0}; //1 indirect
+	uint64_t double_indirect_addresses[DOUBLE_INDIRECT_ADDRESS_COUNT] = {0}; //1 indirect
 	std::bitset<11> inode_bits(0LL); //rwxrwxrwx (ow, g, oth) dir special 
 
 	SuperBlock *superblock;	
 
+	std::shared_ptr<Chunk> resolve_indirection(uint64_t chunk_number){
+		const uint64_t num_chunk_address_per_chunk = superblock->disk_chunk_size / sizeof(uint64_t);
+		uint64_t indirect_address_count = 1;
+	
+		uint64_t *indirect_table = addresses;
+		for(uint64_t indirection = 0; indirection < sizeof(INDIRECT_TABLE_SIZES) / sizeof(uint64_t); indirection++){
+			if(chunk_number < (indirect_address_count * INDIRECT_TABLE_SIZES[indirection])){
+				uint64_t next_chunk_loc = indirect_table[chunk_number / indirect_address_count];
+				if(next_chunk_loc == 0){
+					return nullptr;
+				}
+				auto chunk = superblock->disk->get_chunk(next_chunk_loc);
+				while(indirection != 0){
+					uint64_t *lookup_table = chunk->data->get();
+					next_chunk_loc = lookup_table[chunk_number % indirect_address_count];
+					if(next_chunk_loc == 0){
+						return nullptr;
+					}
+					indirect_address_count /= num_chunk_address_per_chunk;
+					chunk = superblock->disk->get_chunk(next_chunk_loc);
+					indirection--;
+				}
+				return chunk;
+			}
+			chunk_number -= (indirect_address_count * INDIRECT_TABLE_SIZES[indirection]);
+			indirect_table += INDIRECT_TABLE_SIZES[indirection];
+			indirect_address_count *= num_chunk_address_per_chunk;
+		}
+		return nullptr;
+	}
+
+	/**
+	std::shared_ptr<Chunk> get_chunk(uint64_t chunk_number){
+		const uint64_t num_chunk_address_per_chunk = superblock->disk_chunk_size / sizeof(uint64_t);
+		if(chunk_number < DIRECT_ADDRESS_COUNT){ //access direct blocks
+			if(direct_addresses[chunk_number] == 0){
+				throw FileSystemException("INode attempt to access chunk that does not exist");
+			}
+			return superblock->disk->get_chunk(direct_address[chunk_number]);
+		}else if(chunk_number < (DIRECT_ADDRESS_COUNT + (num_chunk_address_per_chunk * INDIRECT_ADDRESS_COUNT))){ //access indirect block
+			chunk_number -= DIRECT_ADDRESS_COUNT;
+			uint64_t indirect_block_number = chunk_number / num_chunk_address_per_chunk;
+			if(indirect_addresses[indirect_block_number] == 0){
+				throw FileSystemException("INode attempt to access chunk that does not exist");
+			}
+			auto indirect_block = superblock->disk->get_chunk(indirect_address[indirect_block_number]);
+			uint64_t *indirect_addresses = (uint64_t*)indirect_block->data->get();
+			if(indirect_addresses[chunk_number % num_chunk_address_per_chunk] == 0){
+				throw FileSystemException("INode attempt to access chunk that does not exist");
+			}
+		}else{ //access double indirect access
+			if(direct_addresses[chunk_number] == 0){
+				throw FileSystemException("INode attempt to access chunk that does not exist");
+			}
+			superblock->disk->get_chunk(direct_address[chunk_number]);
+		}
+	}
+	**/
+
 	uint64_t read(uint64_t starting_offset, char *buf, uint64_t n){
-		uint64_t chunk_number = starting_offset / superblock->chunk_size;
-		uint64_t byte_offset = starting_offset % superblock->chunk_size;
-		//split into function
-		if(chunk_number < 8){ //access direct blocks
-			
-		}else if(chunk_number < (8 + (superblock->chunk_size / sizeof(uint64_t)))){//change to var
-			
-		}																
+		uint64_t chunk_number; //index of chunk
+		uint64_t byte_offset; //index of byte within that chunk
+		uint64_t starting_offset_chunk; //starting byte offset of chunk
+		uint64_t ending_offset_chunk; //ending byte offset of chunk
+		uint64_t num_chunk_address_per_chunk = superblock->chunk_size / sizeof(uint64_t);
+		uint64_t num_bytes_till_end_of_chunk; //including byte_offset
+		uint64_t num_of_chunks_to_access; //number of chunks to access
+		uint64_t i; //loop counter
+		uint64_t bytes_to_read;
+		std::shared_ptr<Chunk> chunk;
+
+		//setup info for first chunk
+		chunk_number = starting_offset / superblock->chunk_size;
+		byte_offset = starting_offset % superblock->chunk_size;
+		starting_offset_chunk = chunk_number * superblock->chunk_size;
+		ending_offset_chunk = starting_offset_chunk + superblock->chunk_size - 1;
+		num_bytes_till_end_of_chunk = superblock->chunk_size - byte_offset;
+
+		//find number of chunks to access
+		num_of_chunks_to_access = 1;
+		if(n > num_bytes_till_end_of_chunk){
+			n_temp = n - num_bytes_till_end_of_chunk;
+			num_of_chunks_to_access += (n_temp / superblock->chunk_size);
+		}
+
+		for(i = 0; i < num_of_chunks_to_access; i++){
+			if(n <= num_bytes_till_end_of_chunk){
+				bytes_to_read = n;
+			}else{
+				bytes_to_read = num_bytes_till_end_of_chunk;
+			}
+			n -= bytes_to_read;
+
+			//split into function
+			chunk = disk->get_chunk(chunk_number);
+			std::memcpy((void*)chunk->get(), buf, bytes_to_read);
+			chunk_number += 1;
+			byte_offset = 0;
+			starting_offset_chunk = chunk_number * superblock->chunk_size;
+			ending_offset_chunk = starting_offset_chunk + superblock->chunk_size - 1;
+			num_bytes_till_end_of_chunk = superblock->chunk_size - byte_offset;
+		}
 	}
 
 };
