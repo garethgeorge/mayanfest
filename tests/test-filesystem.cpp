094fa904eda02bae18858f394b3ef40ef2a00657
root <root@euca-10-1-5-164.eucalyptus.internal>
2018-11-09 02:07:21 +0000

    implemented some more substantial tests -- all seem to pass

--- tests/test-filesystem.cpp
+++ tests/test-filesystem.cpp
@@ -96,42 +96,87 @@ TEST_CASE( "INodes read/write should work on a small disk with reasonably sized 
 	}
 
 	SECTION("INodes can be written short strings at VERY LARGE offsets (a few pages)"){
-		try{
-			std::cout << std::endl << std::endl;
-			INode inode;
-			REQUIRE(fs->superblock->disk == disk.get());
-			inode.superblock = fs->superblock.get();
-			REQUIRE(inode.superblock->disk == disk.get());
-			
-			char str[] = "hello there!";
-			REQUIRE(inode.write(10 * 1024 * 1024, str, sizeof(str)) == sizeof(str));
+		std::cout << std::endl << std::endl;
+		INode inode;
+		REQUIRE(fs->superblock->disk == disk.get());
+		inode.superblock = fs->superblock.get();
+		REQUIRE(inode.superblock->disk == disk.get());
+		
+		char str[] = "hello there!";
+		REQUIRE(inode.write(10 * 1024 * 1024, str, sizeof(str)) == sizeof(str));
 
-			char buf[sizeof(str)];
-			REQUIRE(inode.read(10 * 1024 * 1024, buf, sizeof(str)) == sizeof(str));
+		char buf[sizeof(str)];
+		REQUIRE(inode.read(10 * 1024 * 1024, buf, sizeof(str)) == sizeof(str));
+		REQUIRE(strcmp(buf, str) == 0);
+	}
+
+	SECTION("Can write a 10KB file & read it back successfully"){
+		std::cout << std::endl << std::endl;
+		INode inode;
+		REQUIRE(fs->superblock->disk == disk.get());
+		inode.superblock = fs->superblock.get();
+		REQUIRE(inode.superblock->disk == disk.get());
+		
+		char str[] = "Blanditiis pariatur alias distinctio aut. "
+			"Occaecati et incidunt nesciunt dolorum est id excepturi. "
+			"Sunt quod sapiente consequatur voluptate rerum voluptatum "
+			"harum.";
+		for (size_t i = 0; i < 10 * 1024; i += sizeof(str)) {
+			REQUIRE(inode.write(i, str, sizeof(str)) == sizeof(str));
+		}
+
+		char buf[sizeof(str)];
+		
+		for (size_t i = 0; i < 10 * 1024; i += sizeof(str)) {
+			REQUIRE(inode.read(i, buf, sizeof(str)) == sizeof(str));
 			REQUIRE(strcmp(buf, str) == 0);
-		}catch(const FileSystemException &e){
-			std::cout<< e.message << std::endl;
-		}catch(const DiskException &e) {
-			std::cout<< e.message << std::endl;
 		}
 	}
 
-	SECTION("Can write a 10KB file"){
-		try{
-			std::cout << std::endl << std::endl;
-			INode inode;
-			REQUIRE(fs->superblock->disk == disk.get());
-			inode.superblock = fs->superblock.get();
-			REQUIRE(inode.superblock->disk == disk.get());
-			
-			char str[] = "hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!";
-			for (size_t i = 0; i < 10 * 1024; i += sizeof(str)) {
-				REQUIRE(inode.write(i, str, sizeof(str)) == sizeof(str));	
+	SECTION("Can write a 100KB file & read it back successfully"){
+		std::cout << std::endl << std::endl;
+		INode inode;
+		REQUIRE(fs->superblock->disk == disk.get());
+		inode.superblock = fs->superblock.get();
+		REQUIRE(inode.superblock->disk == disk.get());
+		
+		char str[] = "Blanditiis pariatur alias distinctio aut. "
+			"Occaecati et incidunt nesciunt dolorum est id excepturi. "
+			"Sunt quod sapiente consequatur voluptate rerum voluptatum "
+			"harum.";
+		for (size_t i = 0; i < 100 * 1024; i += sizeof(str)) {
+			if (inode.write(i, str, sizeof(str)) != sizeof(str)) {
+				REQUIRE(false);
 			}
-		}catch(const FileSystemException &e){
-			std::cout<< e.message << std::endl;
-		}catch(const DiskException &e) {
-			std::cout<< e.message << std::endl;
+		}
+
+		char buf[sizeof(str)];
+		
+		for (size_t i = 0; i < 100 * 1024; i += sizeof(str)) {
+			if (inode.read(i, buf, sizeof(str)) != sizeof(str)) {
+				REQUIRE(false);
+			}
+			if (strcmp(buf, str) != 0) {
+				REQUIRE(false);
+			}
 		}
 	}
+
+	SECTION("Can write a single, very VERY large string all at once"){
+		std::cout << std::endl << std::endl;
+		INode inode;
+		REQUIRE(fs->superblock->disk == disk.get());
+		inode.superblock = fs->superblock.get();
+		REQUIRE(inode.superblock->disk == disk.get());
+
+		char *message = malloc(10 * 1024 + 1);
+		for (size_t i = 0; i < 10 * 1024; ++i) {
+			message[i] = 'a' + i % 26;
+		}
+		message[10 * 1024] = 0;
+
+		// TODO: write out the very big string
+	}
+
+	// TODO: add test for case where disk runs out of space, see what happens
 }
