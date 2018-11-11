574ed2802da9c5b084a5c72677911971d7e9dd63
Thomas Schibler <thomasSchibler@169-231-112-144.wireless.ucsb.edu>
2018-11-11 12:47:56 -0800

    added path parsing

--- src/syscall.hpp
+++ src/syscall.hpp
@@ -2,43 +2,38 @@
 #define SYSCALL_HPP
 
 #include <filesystem.hpp>
+#include <vector>
+#include <string>
 
 struct MockSyscalls {
     FileSystem * fs;
 
-    int mknod(const char * pathname) {
+    std::vector<std::string> parse_path(const char * pathname) {
         if(*pathname != '/') {
             throw FileSystemException("relative file paths not supported");
         } else {
             pathname += 1;
         }
+
         const char * delimiter;
-        char path_segment[256];
-        path_segment[0] = '\0';
         int iteration = 0;
-        while(delimiter = strchr(pathname, '/')) {
-            strncpy(path_segment, pathname, delimiter - pathname);
-            path_segment[delimiter - pathname] = '\0';   
-            std::cout << iteration << ", pathname: " << pathname << std::endl;
-            std::cout << iteration << ", segment: " << path_segment << std::endl;
-            std::cout << iteration << ", delimiter: " << delimiter << std::endl;
-            if(delimiter[1] == '\0') {
-                path_segment[delimiter - pathname - 1] = '\0';
-                break;
-            }       
-            iteration++;
+        std::vector<std::string> parsed_path;
+        while(delimiter = strchr(pathname, '/') ) {
+            if(delimiter != pathname) {
+                parsed_path.push_back(std::string(pathname, delimiter - pathname));
+                iteration++;
+            }
             pathname = delimiter + 1;
             //Check for directory exists
         }
-        std::cout << iteration << ", pathname: " << pathname << std::endl;
-        std::cout << iteration << ", segment: " << path_segment << std::endl;
-        std::cout << iteration << ", delimiter: " << delimiter << std::endl;
+        if(strlen(pathname) > 0) {
+            parsed_path.push_back(std::string(pathname));
+        }
+        return parsed_path;
+    }
 
-        //We found path, directory to be created is path_segment
-        //INode node = fs->superblock->inode_table->alloc_inode();
-        //uint64_t file_size = 0;
-        //uint64_t reference_count = 1;
-
+    int mknod(const char * pathname) {
+        
         return 0;
     }
 };
